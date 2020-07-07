202b19a2610bc6f4fef1413c712a3d7b28cbd77f
unknown <hfa1377@gmail.com>
2020-07-08 01:20:48 +0430

    Edit proc structure and Add some fields to it

--- proc.h
+++ proc.h
@@ -49,6 +49,11 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int stime;                   // Process start time
+  int etime;                   // Process end time
+  int rtime;                   // Process run time
+  int iotime;                  // Process i/o time               
+
 };
 
 // Process memory is laid out contiguously, low addresses first:
@@ -56,10 +61,3 @@ struct proc {
 //   original data and bss
 //   fixed-size stack
 //   expandable heap
-
-
-
-struct proc_info {
-    int pid;
-    int memsize;
- };
