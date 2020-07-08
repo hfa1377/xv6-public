6e69f8980c9a2c425741dcc33fe1b7e61a8d435a
unknown <hfa1377@gmail.com>
2020-07-08 16:25:07 +0430

    Add interface of set_priority in user.h file

--- user.h
+++ user.h
@@ -23,7 +23,8 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
-int getallP(void);
+int pstatus(void);
+int set_priority(int value);
 
 // ulib.c
 int stat(const char*, struct stat*);
