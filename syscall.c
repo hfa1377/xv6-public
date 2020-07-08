8bc66a4eeb90e25295cca05656a453415151db1f
unknown <hfa1377@gmail.com>
2020-07-08 16:31:26 +0430

    Add set_priority syscall to syscalls

--- syscall.c
+++ syscall.c
@@ -103,7 +103,8 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
-extern int sys_getallP(void);
+extern int sys_pstatus(void);
+extern int sys_set_priority(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -127,7 +128,8 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
-[SYS_getallP] sys_getallP,
+[SYS_pstatus] sys_pstatus,
+[SYS_set_priority] sys_set_priority,
 };
 
 void
