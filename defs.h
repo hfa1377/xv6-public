2b3d3b831cce908890d3bc8491b22a6fc82bc777
unknown <hfa1377@gmail.com>
2020-07-08 16:26:45 +0430

    Add syscall to define

--- defs.h
+++ defs.h
@@ -120,7 +120,8 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
-void 			getallP(void);
+int             pstatus(void);
+int             set_priority(int value);
 // swtch.S
 void            swtch(struct context**, struct context*);
 
