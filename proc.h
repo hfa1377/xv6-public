2dc07e5f6561a51cd37da4a187fce4676e7adad5
unknown <hfa1377@gmail.com>
2020-07-08 16:27:57 +0430

    Add priority to proc structure

--- proc.h
+++ proc.h
@@ -49,6 +49,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int priority;                // Process priority
 };
 
 // Process memory is laid out contiguously, low addresses first:
@@ -56,10 +57,3 @@ struct proc {
 //   original data and bss
 //   fixed-size stack
 //   expandable heap
-
-
-
-struct proc_info {
-    int pid;
-    int memsize;
- };
